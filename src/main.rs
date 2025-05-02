ddf9286e5ac3175b24d67d309137290bfe49b441
pando <sooi.briek@gmail.com>
2025-05-03 02:21:30 +0300

    complete main.rs have working file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+use std::net::UdpSocket;
+
 mod dns;
+use dns::Header;
 
 fn main() {
-    println!("Hello, world!");
+    let socket = UdpSocket::bind("0.0.0.0:1053").expect("Could not bind to port 1053");
+    let mut buf = [0; 512];
+
+    println!("DNS server is running at port 1053");
+
+    loop {
+        let (len, addr) = socket.recv_from(&mut buf).expect("Could not receive data");
+        let header = Header::from_bytes(&buf[..len]).expect("Could not parse DNS header");
+        println!("Received query from {} {:?}", addr, header);
+    }
 }
